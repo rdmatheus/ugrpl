9d05603eb3942852fefa47aa51e5260f22aa894b
rdmatheus <rodrigo.matheus@live.com>
2024-06-11 16:49:42 -0300

    Add the 'bats' dataset and its documentation

---
+++ R/bats.R
@@ -0,0 +1,25 @@
+#' Percent Body Fat of Little Brown Bats from Aeolus Cave
+#'
+#' This dataset contains information on the percent body fat of little brown bats (\emph{Myotis lucifugus})
+#'     sampled in Aeolus Cave, East Dorset, Vermont, located in the northeastern United States.
+#'
+#' @usage data(bats)
+#'
+#' @format A data frame with 159 rows and 4 variables:
+#' \describe{
+#'   \item{sex}{sex of the sampled bat, with \code{F} for female and \code{M} for male.}
+#'   \item{percentfat}{percent body fat.}
+#'   \item{year}{year the bat was sampled.}
+#'   \item{days}{hibernation time, defined as the number of days since the fall equinox.}
+#' }
+#'
+#' @details
+#' This subset of data was collected by Cheng et al. (2019) specifically from Aeolus Cave, one of
+#'     five regions studied in the United States. The dataset provides insight into the fat stores
+#'     of bats, which is crucial for understanding their persistence in the face of white-nose syndrome.
+#'
+#' @source \url{https://datadryad.org/stash/dataset/doi:10.5061/dryad.sh487nh}
+#'
+#' @references
+#' Cheng TL, Gerson A, Moore MS, et al. (2019). Higher fat stores contribute to persistence of little brown bat populations with white-nose syndrome. \emph{Journal of Animal Ecology}. \bold{88}, 591--600
+"bats"
